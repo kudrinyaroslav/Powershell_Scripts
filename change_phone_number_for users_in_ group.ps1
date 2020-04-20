b477face83c9b6a738f22418f2924c349634579f
yaroslav.kudrin <yaroslav.kudrin@mir.dev>
2020-04-20 11:32:27 +0300

    add script for change phonenumber

---
+++ change_phone_number_for users_in_ group.ps1
@@ -0,0 +1,6 @@
+﻿#change telnumber for users
+$group = get-adgroup -filter "name -like '*МИР Сотрудники'" 
+get-adgroupmember -id $group -recursive | foreach {get-aduser $_ }
+foreach($user in $users){
+set-aduser -id $user -OfficePhone "+7 (812) 660-67-01"
+}
