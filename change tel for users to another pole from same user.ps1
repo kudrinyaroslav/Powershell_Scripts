7896f4ecd1631b2ba73a70b0610a674f40c6e5ab
yaroslav.kudrin <yaroslav.kudrin@mir.dev>
2020-04-24 18:23:39 +0300

    add script for change attribute on another in user property

---
+++ change tel for users to another pole from same user.ps1
@@ -0,0 +1,14 @@
+﻿#change telnumber for users to another pole from same user
+
+#get group from AD
+$group = get-adgroup -filter "name -like '*МИР Сотрудники'" 
+
+#get userlist as objects from group 
+$users = get-adgroupmember -id $group -recursive | foreach {get-aduser $_ -properties *}
+
+
+#for each user set pole on another
+foreach($user in $users){
+$userphone = $user.ipphone
+set-aduser -id $user -OfficePhone $userphone
+}
